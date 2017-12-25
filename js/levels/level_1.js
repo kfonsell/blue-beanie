96d6f41d3e6375fd8f515fa1f9a743d9b9998994
faraco <skelic3@gmail.com>
2017-12-26 01:48:06 +0800

    Finishing level 1 core prototype.
    - Will add enemies and door mechanism next commit.

--- js/levels/level_1.js
+++ js/levels/level_1.js
@@ -7,6 +7,7 @@ class level_1_State
     {
         this.map = game.add.tilemap('map');
         this.map.addTilesetImage('landscape');
+        this.map.addTilesetImage('key');
 
         this.layer_0 = this.map.createLayer('background_layer');
         this.layer_0.resizeWorld();
@@ -21,22 +22,44 @@ class level_1_State
 
         this.beanie = game.add.sprite(256, 256, 'beanie');
         game.physics.arcade.enable(this.beanie);
+        
+        this.keys = game.add.group();
+        this.keys.enableBody = true;
 
-        this.player.collideWorldBounds = true;
+        this.map.createFromObjects('objects_layer', 65, 'key', 0, true, false, this.keys);
+
+        this.collected_keys = 0;
     }
 
     update()
     {
         game.physics.arcade.collide(this.player, this.layer_0);
-        game.physics.arcade.overlap(this.player, this.beanie, this.win_state, null, this);
+        game.physics.arcade.overlap(this.player, this.beanie, this.level_2_state, null, this);
+        game.physics.arcade.overlap(this.player, this.keys, this.collect_key, null, this);
 
         /* Stop player from going outside the map through the wall */
 
         this.player_controller.set_main_player_movements();
     }
 
-    win_state()
+    collect_key(player, key)
     {
-        game.state.start('win');
+        this.collected_keys += 1;
+        key.kill();
+        console.log(this.collected_keys);
+    }
+
+    level_2_state()
+    {
+
+        if (this.collected_keys === 3)
+        {
+
+            /* TODO Implement level 2.
+             *
+             * Temporary placeholder for level 2
+             */
+            game.state.start('win');
+        }
     }
 }
