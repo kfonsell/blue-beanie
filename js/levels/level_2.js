2209034bb640b6ebecfca55bc4a8c9a33c305827
faraco <skelic3@gmail.com>
2017-12-27 18:51:59 +0800

    Make enemies 'alive'.

--- js/levels/level_2.js
+++ js/levels/level_2.js
@@ -10,6 +10,7 @@ class level_2_State
         this.map.addTilesetImage('landscape');
         this.map.addTilesetImage('lava');
         this.map.addTilesetImage('key');
+        this.map.addTilesetImage('bat');
 
         this.layer_0 = this.map.createLayer('background_layer');
         this.layer_0.resizeWorld();
@@ -28,20 +29,43 @@ class level_2_State
 
         this.map.createFromObjects('collectables_layer', 74, 'key', 0, true, false, this.keys);
         this.collected_keys = 0;
+
+        this.enemies = game.add.group();
+        this.enemies.enableBody = true;
+
+        this.map.createFromObjects('enemies_layer', 76, 'bat', 1, true, false, this.enemies);
+        this.enemies.callAll('animations.add', 'animations', 'fly', [1, 2, 3], 10, true);
+        this.enemies.callAll('animations.play', 'animations', 'fly');
     }
 
     update()
     {
         game.physics.arcade.collide(this.player, this.layer_0);
         game.physics.arcade.overlap(this.player, this.keys, this.collect_key, null, this);
+        game.physics.arcade.overlap(this.player, this.enemies, this.restart_level, null, this);
 
         this.player_controller.set_main_player_movements();
+
+        let player = this.player;
+        this.enemies.forEachAlive(function(enemy)
+        {
+            if (enemy.visible && enemy.inCamera)
+            {
+                game.physics.arcade.moveToObject(enemy, player, enemy.speed);
+            }
+
+        });
     }
 
     collect_key(player, key)
     {
         this.collected_keys += 1;
         key.kill();
+    }
+
+    restart_level()
+    {
+        game.state.start('lose');
     }
 
     goto_level_2()
