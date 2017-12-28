358d207640038f53daf16e4c0f948bbf1b1a71f5
faraco <skelic3@gmail.com>
2017-12-29 00:37:24 +0800

    Add keys.

--- js/levels/level_5/level_5.js
+++ js/levels/level_5/level_5.js
@@ -26,16 +26,32 @@ class level_5_State
     {
         this.map = game.add.tilemap('map_5');
         this.map.addTilesetImage('landscape');
+        this.map.addTilesetImage('lava');
+        this.map.addTilesetImage('key');
 
         this.layer_0 = this.map.createLayer('background_layer');
         this.layer_0.resizeWorld();
 
+        this.map.setCollision(45);
+        this.map.setCollision(44);
+        this.map.setCollision(34);
+        this.map.setCollision(35);
+
         this.player = game.add.sprite(game.world.centerX + 30, game.world.centerY, 'player');
         this.player_controller = new gameUtils(this.player);
+
+        this.keys = game.add.group();
+        this.keys.enableBody = true;
+
+        this.map.createFromObjects('collectables_layer', 74, 'key', 0, true, false, this.keys);
+        this.collected_keys = 0;
     }
 
     update()
     {
+        /* Block player from going outside the map */
+        game.physics.arcade.collide(this.player, this.layer_0);
+
         this.player_controller.set_main_player_movements();
     }
 }
