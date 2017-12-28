986a3468a3b3616b1a3cca5378ce98882e457d8f
faraco <skelic3@gmail.com>
2017-12-29 00:44:46 +0800

    Add enemies.

--- js/levels/level_5/level_5.js
+++ js/levels/level_5/level_5.js
@@ -28,6 +28,8 @@ class level_5_State
         this.map.addTilesetImage('landscape');
         this.map.addTilesetImage('lava');
         this.map.addTilesetImage('key');
+        this.map.addTilesetImage('bat');
+        this.map.addTilesetImage('wm');
 
         this.layer_0 = this.map.createLayer('background_layer');
         this.layer_0.resizeWorld();
@@ -35,7 +37,7 @@ class level_5_State
         this.map.setCollision(45);
         this.map.setCollision(44);
         this.map.setCollision(34);
-        this.map.setCollision(35);
+        this.map.setCollision(38);
 
         this.player = game.add.sprite(game.world.centerX + 30, game.world.centerY, 'player');
         this.player_controller = new gameUtils(this.player);
@@ -45,6 +47,18 @@ class level_5_State
 
         this.map.createFromObjects('collectables_layer', 74, 'key', 0, true, false, this.keys);
         this.collected_keys = 0;
+
+        this.enemies1 = game.add.group();
+        this.enemies1.enableBody = true;
+
+        this.map.createFromObjects('enemies_layer1', 76, 'bat', 1, true, false, this.enemies1);
+        this.enemies1.callAll('animations.add', 'animations', 'fly', [1, 2, 3], 10, true);
+        this.enemies1.callAll('animations.play', 'animations', 'fly');
+
+        this.enemies2 = game.add.group();
+        this.enemies2.enableBody = true;
+
+        this.map.createFromObjects('enemies_layer2', 91, 'wm', 0, true, false, this.enemies2);
     }
 
     update()
@@ -53,5 +67,16 @@ class level_5_State
         game.physics.arcade.collide(this.player, this.layer_0);
 
         this.player_controller.set_main_player_movements();
+
+        /* Make bats follow player */
+        let player = this.player;
+        this.enemies1.forEachAlive(function(enemy)
+        {
+            if (enemy.visible && enemy.inCamera)
+            {
+                game.physics.arcade.moveToObject(enemy, player, enemy.speed);
+            }
+
+        });
     }
 }
